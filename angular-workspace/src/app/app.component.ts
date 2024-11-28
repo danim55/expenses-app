03abcbe83eb0d8a4388fa08a68da8169ca18137b
dani <daniel.merino@thalesaleniaspace.com>
2024-11-29 00:36:34 +0100

    Add common module

--- angular-workspace/src/app/app.component.ts
+++ angular-workspace/src/app/app.component.ts
@@ -1,14 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [CommonModule, RouterModule, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'expenses-app';
+
 }
