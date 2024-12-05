946f2ac5c8b84ce6b155a3a512857c30c041e051
dani <daniel.merino@thalesaleniaspace.com>
2024-12-05 22:10:35 +0100

    Upgrade to v19

--- angular-workspace/src/app/app.component.ts
+++ angular-workspace/src/app/app.component.ts
@@ -4,11 +4,10 @@ import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterModule, HeaderComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    imports: [CommonModule, RouterModule, HeaderComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'expenses-app';
